2be6d141d398101808019b6d0134b7800846737f
陈修宏 <chenxh.cz@gmail.com>
2018-01-03 14:21:17 +0800

    解题：判断路线圈

--- src/sample/Solution.java
+++ src/sample/Solution.java
@@ -13,6 +13,27 @@ public class Solution {
         System.out.println(sumString("119999", "990001"));
     }
 
+    /**
+     * Judge Route Circle
+     * @param moves
+     * @return
+     */
+    public boolean judgeCircle(String moves) {
+        int x=0 ,y=0;
+        for (char move: moves.toCharArray()) {
+            if (move == 'U'){
+                y--;
+            }else if(move == 'D'){
+                y++;
+            }else if(move == 'L'){
+                x--;
+            }else if (move == 'R'){
+                x++;
+            }
+        }
+        return x == 0 && y==0;
+    }
+
 //    function sum(str1, str2) {
 //        let res = '',
 //                next = 0
